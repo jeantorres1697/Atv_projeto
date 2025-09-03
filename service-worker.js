0b21a4da01ca58e62247a93061232592b7e325eb
Jean Torres <88167764+jeantorres1697@users.noreply.github.com>
2025-09-02 21:47:30 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -20,17 +20,6 @@ self.addEventListener("install", event => {
     self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-            );
-        })
-    );
-    self.clients.claim();
-});
-
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(response => {
@@ -39,4 +28,5 @@ self.addEventListener("fetch", event => {
     );
 
 });
+
 

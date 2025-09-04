f65e6e9f75d0d91107871d9357fbde60f705cf72
Jean Torres <88167764+jeantorres1697@users.noreply.github.com>
2025-09-04 19:38:13 -0300

    Update service-worker.js
    alteração para offline e correção no F5 com comentário.

--- service-worker.js
+++ service-worker.js
@@ -11,6 +11,8 @@ const FILES_TO_CACHE = [
     "/sdr/image/plano_fundo.jpg"
 ];
 
+
+// Instalação dos arquivos no cache
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -20,13 +22,40 @@ self.addEventListener("install", event => {
     self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-
+// Ativação e remoção dos caches antigos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
-
+// Cache para evitar problemas com a tecla F5
+self.addEventListener("fetch", (event) => {
+    if (event.request.mode === "navigate") {
+        // Para navegação (HTML) 
+        event.respondWith(
+            fetch(event.request).catch(() =>
+                caches.match(event.request).then((resp) => resp || caches.match())
+            )
+        );
+    // Para  arquivos estáticos → Cache First
+    } else {
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+                return (
+                    resp ||
+                    fetch(event.request).then((response) => {
+                        return caches.open(CACHE_NAME).then((cache) => {
+                            cache.put(event.request, response.clone());
+                            return response;
+                        });
+                    })
+                );
+            })
+        );
+    }
+})
+    
